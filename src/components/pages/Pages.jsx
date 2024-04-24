d435e89cd637570e9d43dd5826ac900f42c4fd1a
Vakho <vakho.daraselia@gmail.com>
2024-04-25 00:42:50 +0400

    restyle some components, add lightbox, fix some bugs

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,29 +1,27 @@
-import React from "react"
-import Header from "../common/header/Header"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
-import Home from "../home/Home"
-import Footer from "../common/footer/Footer"
-import About from "../about/About"
-import Blog from "../blog/Blog"
-import Services from "../services/Services"
-import Contact from "../contact/Contact"
+import React from "react";
+import Header from "../common/header/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "../home/Home";
+import Footer from "../common/footer/Footer";
+import About from "../about/About";
+import Blog from "../blog/Blog";
+import Services from "../services/Services";
+import Contact from "../contact/Contact";
 
 const Pages = () => {
   return (
     <>
       <Router>
         <Header />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/services' component={Services} />
-          <Route exact path='/blog' component={Blog} />
-          <Route exact path='/contact' component={Contact} />
-        </Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/projects" component={Blog} />
+        <Route exact path="/contact" component={Contact} />
         <Footer />
       </Router>
     </>
-  )
-}
+  );
+};
 
-export default Pages
+export default Pages;
